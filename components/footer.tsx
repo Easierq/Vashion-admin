e46468ca98003bec84c42ee99ced60ba099ed539
Easierq <isiaqofficial@gmail.com>
2025-02-24 05:12:56 +0100

    initial commit

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray border-t dark:text-white">
+      <div className="mx-auto py-5">
+        <p className="text-center text-xs text-black font-bold dark:text-white">
+          &copy; 2025 GI , Inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
